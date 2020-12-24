cb1be0be60d6998baef242374326d8e07625a1dd
Sew-a <sevavetisyan97@gmail.com>
2020-12-24 17:01:44 +0400

    add some changes in sample

---
+++ js/sample.js
@@ -0,0 +1,8 @@
+let loc = window.location.pathname;
+
+console.log(loc);
+
+const toggleButton = document.getElementById("color-switch").addEventListener("click", function(){
+   let mode = document.getElementById("mode");
+   document.body.classList.toggle("dark-mode");
+});
