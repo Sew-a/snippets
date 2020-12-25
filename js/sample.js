36f3179089ddcea35619e39356a631dfb3c5e3eb
Sew-a <sevavetisyan97@gmail.com>
2020-12-25 16:07:48 +0400

    Add new functionalitty

--- js/sample.js
+++ js/sample.js
@@ -1,21 +1,25 @@
 let loc = window.location.pathname;
-
-console.log(loc);
+const nav__list = document.getElementById("nav__list");
 
 const toggleButton = document.getElementById("color-switch").addEventListener("click", function(){
    let mode = document.getElementById("mode");
    document.body.classList.toggle("dark-mode");
 });
 
-
-document.getElementById("menu_icon").addEventListener("click", function(elem){
+document.getElementById("menu_icon").addEventListener("click", function(){
    const list_1 = document.getElementById("line-though_1");
    const list_2 = document.getElementById("line-though_2");
-   const nav__list = document.getElementById("nav__list");
 
    list_1.classList.toggle("toggle_line_1");
    list_2.classList.toggle("toggle_line_2");
    
    nav__list.classList.toggle("nav__active");
    document.body.classList.toggle("to_bottom");
+});
+
+document.querySelectorAll("a").forEach((item) => {
+   item.addEventListener("click", function(){
+      nav__list.classList.toggle("nav__active");
+      document.body.classList.toggle("to_bottom");
+   });
 });
