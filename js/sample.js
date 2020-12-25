5fe14ca8082a21421ef42425f854d597faa2a88f
Sew-a <sevavetisyan97@gmail.com>
2020-12-25 15:51:27 +0400

    Actualy create page template with dark/light mode

--- js/sample.js
+++ js/sample.js
@@ -5,5 +5,17 @@ console.log(loc);
 const toggleButton = document.getElementById("color-switch").addEventListener("click", function(){
    let mode = document.getElementById("mode");
    document.body.classList.toggle("dark-mode");
-   console.log("clicked");
+});
+
+
+document.getElementById("menu_icon").addEventListener("click", function(elem){
+   const list_1 = document.getElementById("line-though_1");
+   const list_2 = document.getElementById("line-though_2");
+   const nav__list = document.getElementById("nav__list");
+
+   list_1.classList.toggle("toggle_line_1");
+   list_2.classList.toggle("toggle_line_2");
+   
+   nav__list.classList.toggle("nav__active");
+   document.body.classList.toggle("to_bottom");
 });
